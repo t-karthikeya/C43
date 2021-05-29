2d2a3837ee48680a87c75efd18445fdc556b8930
t-karthikeya <78084305+t-karthikeya@users.noreply.github.com>
2021-05-29 07:21:35 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+
+    fill("white");
+    textSize(15);
+    text("CUTNIFON", 700,150);
+    fill("lightblue");
+    text("Hint: Performs a particular task !!", 700,170);
+
+    fill("white");
+    textSize(15);
+    text("ATADASBA", 400,360);
+    fill("lightblue");
+    text("Hint: Stores all Information !!", 400,380);
+    
+
+}
